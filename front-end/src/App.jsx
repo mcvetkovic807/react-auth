51fa7ef7ecdebae8ae4b8ea421fce7e1887b91ee
Milan Cvetkovic <milan.cvetkovic.807@gmail.com>
2026-01-19 12:14:55 +0100

    03_04b Reset password landing page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -9,6 +9,7 @@ import { useUser } from './useUser';
 import { PleaseVerifyEmailPage } from "./PleaseVerifyEmailPage";
 import { EmailVerificationLandingPage } from "./EmailVerificationLandingPage";
 import { ForgotPasswordPage } from "./ForgotPasswordPage";
+import {PasswordResetLandingPage} from "./PasswordResetLandingPage";
 
 function App() {
   const user = useUser();
@@ -22,6 +23,7 @@ function App() {
           <Route path="/please-verify" element={<PleaseVerifyEmailPage />} />
           <Route path="/verify-email/:verificationString" element={<EmailVerificationLandingPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          <Route path="/reset-password/:passwordResetCode" element={<PasswordResetLandingPage />} />
           <Route element={<PrivateRoute redirectPath="log-in" isAllowed={!!user} />}>
             <Route path="/" element={<UserInfoPage />} />
           </Route>
